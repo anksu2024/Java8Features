86a4895ab1b6b73b2e89c5bef9450d4dd43a5e84
asarraf <sarrafan@Daniels-Laptop.local>
2017-05-21 16:18:19 -0700

    Default Interface Methods

---
+++ src/main/java/com/defaultinterfacemethod/Formula.java
@@ -0,0 +1,19 @@
+package com.defaultinterfacemethod;
+
+/**
+ * Author  : Ankit Sarraf
+ * Dated   : May 21, 2017
+ * Purpose : Interface: 1 unimplemented method and 2 default methods
+ */
+
+public interface Formula {
+    double calculateExpression(int x, int y);
+
+    default double sqrt(int x) {
+        return Math.sqrt(x);
+    }
+
+    default double square(int x) {
+        return Math.pow(x, 2);
+    }
+}
---
+++ src/main/java/com/defaultinterfacemethod/UnderstandingDefaultMethods.java
@@ -0,0 +1,29 @@
+package com.defaultinterfacemethod;
+
+/**
+ * Author  : Ankit Sarraf
+ * Dated   : May 21, 2017
+ * Purpose : To understand default interface methods
+ */
+
+public class UnderstandingDefaultMethods {
+    public static void main(String ... args) {
+        Formula formula = new Formula() {
+            // Initiation of unimplemented interface method
+            public double calculateExpression(int x, int y) {
+                // The expression to be calculated: (100x) ^ 0.5 + y ^ 2
+                return sqrt(100 * x) + square(y);
+            }
+        };
+
+        int x = 64;
+        int y = 20;
+
+        // Using the calculateExpression method
+        System.out.println("Result of calculate(64, 20): " + formula.calculateExpression(x, y));
+
+        // The default interface methods be used out of box as well
+        System.out.println("Result of sqrt(64): " + formula.sqrt(x));
+        System.out.println("Result of square(20): " + formula.square(y));
+    }
+}
